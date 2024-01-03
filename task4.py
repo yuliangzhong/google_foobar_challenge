46647ce46d886e1d9792b218e94c9a14678ade67
yuliangzhong <yubaby2003@163.com>
2024-01-03 20:56:48 +0100

    the grandest staircase of them all

--- task4.py
+++ task4.py
@@ -1,3 +1,49 @@
+# The Grandest Staircase Of Them All
+# ==================================
+# With the LAMBCHOP doomsday device finished, Commander Lambda is preparing to debut on the galactic
+# stage -- but in order to make a grand entrance, Lambda needs a grand staircase! As the Commander's
+# personal assistant, you've been tasked with figuring out how to build the best staircase EVER. 
+
+# Lambda has given you an overview of the types of bricks available, plus a budget. You can buy
+# different amounts of the different types of bricks (for example, 3 little pink bricks, or 5 blue
+# lace bricks). Commander Lambda wants to know how many different types of staircases can be built
+# with each amount of bricks, so they can pick the one with the most options.
+
+# Each type of staircase should consist of 2 or more steps. No two steps are allowed to be at the
+# same height - each step must be lower than the previous one. All steps must contain at least one
+# brick. A step's height is classified as the total amount of bricks that make up that step.
+# For example, when N = 3, you have only 1 choice of how to build the staircase, with the first step
+# having a height of 2 and the second step having a height of 1: (# indicates a brick)
+
+# #
+# ##
+# 21
+
+# When N = 4, you still only have 1 staircase choice:
+
+# #
+# #
+# ##
+# 31
+
+# But when N = 5, there are two ways you can build a staircase from the given bricks. The two
+# staircases can have heights (4, 1) or (3, 2), as shown below:
+
+# #
+# #
+# #
+# ##
+# 41
+
+# #
+# ##
+# ##
+# 32
+
+# Write a function called solution(n) that takes a positive integer n and returns the number of
+# different staircases that can be built from exactly n bricks. n will always be at least 3 (so you
+# can have a staircase at all), but no more than 200, because Commander Lambda's not made of money!
+
 def solution(n):
     # suppose i, j are natural numbers (0, 1, 2, ...)
 
@@ -10,8 +56,7 @@ def solution(n):
     # define h(i, j) = g(i, j) + g(i, j - 1) + ... + g(i, 0)
     # so the sol(i) = h(i, i) - 1
 
-    # we have h(i, j) = g(i, j) + h(i, j - 1)
-    # and from the story, we know
+    # From the story, we know
     # g(i, j) = h(i - j, j - 1)
     
     # ------------------------
@@ -26,10 +71,6 @@ def solution(n):
         for j in range(1, i):
             G[i][j] = sum(G[i - j][k] for k in range(j))
 
-    # ------------------------
-    # print("G table:")
-    # for l in G:
-    #     print(l)
     return sum(G[n][k] for k in range(n + 1)) - 1
 
 print(solution(3))
